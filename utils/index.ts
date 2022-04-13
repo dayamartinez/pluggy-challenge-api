ed66552afac97968f20668025d3ae88e2f4f72e5
Your Name <dayamar4@gmail.com>
2022-04-13 20:09:19 -0300

    handler error

--- utils/index.ts
+++ utils/index.ts
@@ -17,74 +17,86 @@ const parseSellPrice = (sell_price: string | null) =>
   parseFloat(sell_price!.slice(1).replace(",", "."));
 
 const getQuotesOnAmbito = async () => {
-  const apiExt: string = process.env.API_AMBITO_DOLAR;
-  const source: string = process.env.SOURCE_AMBITO;
+  try {
+    const apiExt: string = process.env.API_AMBITO_DOLAR;
+    const source: string = process.env.SOURCE_AMBITO;
 
-  const {
-    data: { compra, venta },
-  } = await axios.get(apiExt);
-  return {
-    sell_price: parseFloat(compra),
-    buy_price: parseFloat(venta),
-    source,
-  };
+    const {
+      data: { compra, venta },
+    } = await axios.get(apiExt);
+    return {
+      sell_price: parseFloat(compra),
+      buy_price: parseFloat(venta),
+      source,
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const getQuotesOnDolarHoy = async () => {
-  const source: string = process.env.SOURCE_DOLAR_HOY;
-  const browser = await chromium.launch({
-    chromiumSandbox: false,
-  });
-  const page = await browser.newPage({
-    bypassCSP: true, // This is needed to enable JavaScript execution on GitHub.
-  });
-  await page.goto(source);
+  try {
+    const source: string = process.env.SOURCE_DOLAR_HOY;
+    const browser = await chromium.launch({
+      chromiumSandbox: false,
+    });
+    const page = await browser.newPage({
+      bypassCSP: true, // This is needed to enable JavaScript execution on GitHub.
+    });
+    await page.goto(source);
 
-  await page.waitForSelector("#sitio", { timeout: 30000 });
+    await page.waitForSelector("#sitio", { timeout: 30000 });
 
-  let buy_price = await page?.$eval(
-    "div[class=compra] div[class=val]",
-    (element) => element?.textContent
-  );
+    let buy_price = await page?.$eval(
+      "div[class=compra] div[class=val]",
+      (element) => element?.textContent
+    );
 
-  let sell_price = await page?.$eval(
-    "div[class=venta] div[class=val]",
-    (element) => element?.textContent
-  );
-  await browser.close();
-  return {
-    sell_price: parseSellPrice(sell_price),
-    buy_price: parseBuyPrice(buy_price),
-    source,
-  };
+    let sell_price = await page?.$eval(
+      "div[class=venta] div[class=val]",
+      (element) => element?.textContent
+    );
+    await browser.close();
+    return {
+      sell_price: parseSellPrice(sell_price),
+      buy_price: parseBuyPrice(buy_price),
+      source,
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const getQuotesOnCronista = async () => {
-  const source: string = process.env.SOURCE_CRONISTA;
-  const browser = await chromium.launch({
-    chromiumSandbox: false,
-  });
-  const page = await browser.newPage({
-    bypassCSP: true, // This is needed to enable JavaScript execution on GitHub.
-  });
-  await page.goto(source);
-  await page.waitForSelector(".main-container", { timeout: 30000 });
+  try {
+    const source: string = process.env.SOURCE_CRONISTA;
+    const browser = await chromium.launch({
+      chromiumSandbox: false,
+    });
+    const page = await browser.newPage({
+      bypassCSP: true, // This is needed to enable JavaScript execution on GitHub.
+    });
+    await page.goto(source);
+    await page.waitForSelector(".main-container", { timeout: 30000 });
 
-  let buy_price = await page?.$eval(
-    "div[class=buy-value]",
-    (ele) => ele?.textContent
-  );
+    let buy_price = await page?.$eval(
+      "div[class=buy-value]",
+      (ele) => ele?.textContent
+    );
 
-  let sell_price = await page?.$eval(
-    "div[class=sell-value]",
-    (ele) => ele?.textContent
-  );
-  await browser.close();
-  return {
-    sell_price: parseSellPrice(sell_price),
-    buy_price: parseBuyPrice(buy_price),
-    source,
-  };
+    let sell_price = await page?.$eval(
+      "div[class=sell-value]",
+      (ele) => ele?.textContent
+    );
+    await browser.close();
+    return {
+      sell_price: parseSellPrice(sell_price),
+      buy_price: parseBuyPrice(buy_price),
+      source,
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default { getQuotesOnAmbito, getQuotesOnCronista, getQuotesOnDolarHoy };
